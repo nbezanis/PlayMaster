2c71909cc658ebec54e209c31e40a355e86dc075
nbezanis <nbezanis98@gmail.com>
2020-01-26 23:11:35 -0500

    split playlist creator into 2 classes

--- lib/playlist_creator.dart
+++ lib/playlist_creator.dart
@@ -5,9 +5,10 @@ import 'main.dart';
 import 'music_list_display.dart';
 import 'app_utils.dart';
 
+//class to hold the providers required for music list display to work
 class PlaylistCreator extends StatelessWidget {
-  MusicInfo musicInfo;
-  SelectInfo selectInfo;
+  final MusicInfo musicInfo;
+  final SelectInfo selectInfo;
 
   PlaylistCreator(this.musicInfo, this.selectInfo);
 
@@ -16,32 +17,53 @@ class PlaylistCreator extends StatelessWidget {
     return ChangeNotifierProvider.value(
       value: musicInfo,
       child: ChangeNotifierProvider.value(
-        value: selectInfo,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: PlayMaster.accentColor,
-            actions: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0.0, 8.0, 8.0, 8.0),
-                child: RaisedButton(
-                  color: Colors.white,
-                  onPressed: () {
-                    //TODO launch name dialog
-                  },
-                  child: Text(
-                    'Add',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                ),
+          value: selectInfo, child: PlaylistContainer()),
+    );
+  }
+}
+
+//class that holds the display of the plalylist creator
+class PlaylistContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var selectInfo = Provider.of<SelectInfo>(context);
+    selectInfo.selecting = true;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: PlayMaster.accentColor,
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0.0, 8.0, 8.0, 8.0),
+            child: RaisedButton(
+              color: Colors.white,
+              onPressed: () => selectInfo.type == Select.all
+                  ? selectInfo.deselectAll()
+                  : selectInfo.selectAll(),
+              child: Text(
+                selectInfo.type == Select.all ? 'deselect all' : 'select all',
+                style: TextStyle(fontSize: 18.0),
               ),
-            ],
+            ),
           ),
-          body: ListView.builder(
-            itemCount: PlayMaster.music.length,
-            itemBuilder: (BuildContext context, int index) =>
-                MusicListDisplay(PlayMaster.music.elementAt(index)),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0.0, 8.0, 8.0, 8.0),
+            child: RaisedButton(
+              color: Colors.white,
+              onPressed: () {
+                //TODO launch name dialog
+              },
+              child: Text(
+                'Add',
+                style: TextStyle(fontSize: 18.0),
+              ),
+            ),
           ),
-        ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: PlayMaster.music.length,
+        itemBuilder: (BuildContext context, int index) =>
+            MusicListDisplay(PlayMaster.music.elementAt(index)),
       ),
     );
   }
